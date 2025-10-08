d19891143348a53f9f5fc1ed899cc579b4cb27c9
spiperac <spiperac@denkei.org>
2025-10-08 23:24:18 +0200

    changed terminal to foot

--- config.h
+++ config.h
@@ -126,7 +126,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[] = { "ghostty", NULL };
+static const char *termcmd[] = { "foot", NULL };
 static const char *menucmd[] = { "wmenu-run", NULL };
 static const char *volumeMute[] = { "pamixer", "-t", NULL };
 static const char *takeScreenshot[] = {
